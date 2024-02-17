12a9acc75973c97478993c470aaef771b4f178ba
saikat <tawhidulislam3482@gmail.com>
2024-02-17 20:09:03 +0600

    footer complete

--- src/Shared/Footer/Footer.jsx
+++ src/Shared/Footer/Footer.jsx
@@ -1,35 +1,54 @@
-
+import logo from "../../assets/images/Screenshot_2024-02-17_192626-removebg-preview.png";
 const Footer = () => {
-    return (
-        <div>
-            <footer className="footer p-10 bg-base-200 text-base-content">
-  <aside>
-    <svg width="50" height="50" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fillRule="evenodd" clipRule="evenodd" className="fill-current"><path d="M22.672 15.226l-2.432.811.841 2.515c.33 1.019-.209 2.127-1.23 2.456-1.15.325-2.148-.321-2.463-1.226l-.84-2.518-5.013 1.677.84 2.517c.391 1.203-.434 2.542-1.831 2.542-.88 0-1.601-.564-1.86-1.314l-.842-2.516-2.431.809c-1.135.328-2.145-.317-2.463-1.229-.329-1.018.211-2.127 1.231-2.456l2.432-.809-1.621-4.823-2.432.808c-1.355.384-2.558-.59-2.558-1.839 0-.817.509-1.582 1.327-1.846l2.433-.809-.842-2.515c-.33-1.02.211-2.129 1.232-2.458 1.02-.329 2.13.209 2.461 1.229l.842 2.515 5.011-1.677-.839-2.517c-.403-1.238.484-2.553 1.843-2.553.819 0 1.585.509 1.85 1.326l.841 2.517 2.431-.81c1.02-.33 2.131.211 2.461 1.229.332 1.018-.21 2.126-1.23 2.456l-2.433.809 1.622 4.823 2.433-.809c1.242-.401 2.557.484 2.557 1.838 0 .819-.51 1.583-1.328 1.847m-8.992-6.428l-5.01 1.675 1.619 4.828 5.011-1.674-1.62-4.829z"></path></svg>
-    <p>ACME Industries Ltd.<br/>Providing reliable tech since 1992</p>
-  </aside> 
-  <nav>
-    <h6 className="footer-title">Services</h6> 
-    <a className="link link-hover">Branding</a>
-    <a className="link link-hover">Design</a>
-    <a className="link link-hover">Marketing</a>
-    <a className="link link-hover">Advertisement</a>
-  </nav> 
-  <nav>
-    <h6 className="footer-title">Company</h6> 
-    <a className="link link-hover">About us</a>
-    <a className="link link-hover">Contact</a>
-    <a className="link link-hover">Jobs</a>
-    <a className="link link-hover">Press kit</a>
-  </nav> 
-  <nav>
-    <h6 className="footer-title">Legal</h6> 
-    <a className="link link-hover">Terms of use</a>
-    <a className="link link-hover">Privacy policy</a>
-    <a className="link link-hover">Cookie policy</a>
-  </nav>
-</footer>
+  return (
+    <div>
+      <footer className="footer p-10 bg-base-200 text-base-content">
+        <aside>
+          <img src={logo} className="" alt="" />
+          <p>
+            ACME Industries Ltd.
+            <br />
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. <br />{" "}
+            Laboriosam quaerat culpa odio consectetur sapiente rem ipsam, <br />{" "}
+            reprehenderit at unde sit?
+          </p>
+          <button className="my-3 btn btn-outline btn-warning">
+            Appointment
+          </button>
+        </aside>
+        <nav>
+          <h6 className="footer-title">Quick Links</h6>
+          <a className="link link-hover">About Us</a>
+          <a className="link link-hover">Service</a>
+          <a className="link link-hover">Doctors</a>
+          <a className="link link-hover">Departments</a>
+          <a className="link link-hover">Online Payment</a>
+          <a className="link link-hover">Contact Us</a>
+        </nav>
+        <nav>
+          <h6 className="footer-title">Doc House Services</h6>
+          <a className="link link-hover">Pediatric Clinic</a>
+          <a className="link link-hover">Diagnosis Clinic</a>
+          <a className="link link-hover">Cardiac Clinic</a>
+          <a className="link link-hover">Laboratory Analysis</a>
+          <a className="link link-hover">Gynecological Clinic</a>
+          <a className="link link-hover">Dental Clinic</a>
+        </nav>
+        <nav>
+          <h6 className="footer-title">Working Hours</h6>
+          <a className="link link-hover">Monday - 10 am to 7 pm</a>
+          <a className="link link-hover">Tuesday - 10 am to 7 pm</a>
+          <a className="link link-hover">Wednesday - 10 am to 7 pm</a>
+          <a className="link link-hover">Thursday - 10 am to 7 pm</a>
+          <a className="link link-hover">Sunday - 10 am to 7 pm</a>
+        </nav>
+        
+      </footer>
+      <div className="text-center bg-gray-100 p-5">
+        Copyright © 2024 - All right reserved by Doc House Ltd
         </div>
-    );
+    </div>
+  );
 };
 
 export default Footer;
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import {
-  RouterProvider,
-} from "react-router-dom";
-import Routes from './Routes/Routes.jsx';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import Routes from "./Routes/Routes.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-   <RouterProvider router={Routes} />
-  </React.StrictMode>,
-)
+    <div className="max-w-screen-xl mx-auto">
+      <RouterProvider router={Routes} />
+    </div>
+  </React.StrictMode>
+);
