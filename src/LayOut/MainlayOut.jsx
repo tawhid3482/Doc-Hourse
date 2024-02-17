ea73290322542a9b20bcb17fb5c6decc8b8e73d0
saikat <tawhidulislam3482@gmail.com>
2024-02-17 20:13:49 +0600

    navber working

--- src/LayOut/MainlayOut.jsx
+++ src/LayOut/MainlayOut.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
 import Footer from "../Shared/Footer/Footer";
+import Navber from "../Shared/Navber/Navber";
 
 const MainlayOut = () => {
     return (
         <div>
+            <Navber></Navber>
             <Outlet></Outlet>
             <Footer></Footer>
         </div>
