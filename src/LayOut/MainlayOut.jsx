6e3fc097fe7993c1e0828d3a651a8da57f2dc144
saikat <tawhidulislam3482@gmail.com>
2024-02-17 19:22:08 +0600

    footer working

--- src/LayOut/MainlayOut.jsx
+++ src/LayOut/MainlayOut.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
+import Footer from "../Shared/Footer/Footer";
 
 const MainlayOut = () => {
     return (
         <div>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
