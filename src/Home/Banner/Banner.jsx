a811497d15d531c978f685fd2ced77cafd5d16b8
saikat <tawhidulislam3482@gmail.com>
2024-02-18 19:43:53 +0600

    banner is complete

--- src/Home/Banner/Banner.jsx
+++ src/Home/Banner/Banner.jsx
@@ -6,12 +6,12 @@ const Banner = () => {
     <div>
      <div  className="relative ">
         <img src={logo} alt="" />
-       <div className="absolute -mt-48 md:-mt-[400px] lg:-mt-[600px] p-5">
-       <div className=" flex justify-between items-center">
+       <div className="absolute -mt-52  md:-mt-[400px] lg:-mt-[600px] p-5">
+       <div className=" flex justify-between items-center ">
         <div className=" ">
             <h1 className='text-base md:text-4xl lg:text-7xl font-bold text-white'>Your Best Medical Help Center</h1>
             <p className='text-white my-4 md:my-8 text-xs '>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Itaque repellendus eligendi  <br /></p>
-            <button className='btn btn-warning'>All Services</button>
+            <button className='btn btn-sm md:btn-md btn-warning'>All Services</button>
         </div>
         <div className="">
             <img src={doc} alt="" />
