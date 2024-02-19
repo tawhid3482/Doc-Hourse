4c2b441eb9883f4f48a0228fee19f74deafe009d
saikat <tawhidulislam3482@gmail.com>
2024-02-19 20:37:07 +0600

    responsive complete

--- src/Home/Banner/Banner.jsx
+++ src/Home/Banner/Banner.jsx
@@ -3,11 +3,11 @@ import doc from '../../assets/Banner/doc.png'
 
 const Banner = () => {
   return (
-    <div>
+    <div className=''>
      <div  className="relative ">
-        <img src={logo} alt="" />
-       <div className="absolute -mt-52  md:-mt-[400px] lg:-mt-[600px] p-5">
-       <div className=" flex justify-between items-center ">
+        <img src={logo} className='h-[480px] md:h-screen' alt="" />
+       <div className="absolute -mt-[460px]  md:-mt-[400px] lg:-mt-[600px] p-5">
+       <div className="flex-col md:flex-row md:flex md:justify-between md:items-center ">
         <div className=" ">
             <h1 className='text-base md:text-4xl lg:text-7xl font-bold text-white'>Your Best Medical Help Center</h1>
             <p className='text-white my-4 md:my-8 text-xs '>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Itaque repellendus eligendi  <br /></p>
