71ca5e2ae9aa88cbcbde6a66ec5491e0126b8009
saikat <tawhidulislam3482@gmail.com>
2024-02-27 18:38:15 +0600

    Doc detaisl about

--- src/Home/Doctor/DocDetailsShow.jsx
+++ src/Home/Doctor/DocDetailsShow.jsx
@@ -1,13 +1,10 @@
 const DocDetailsShow = ({ doc }) => {
   // console.log(doc)
   return (
-    <div>
-      <div className="card card-side bg-base-100 shadow-xl my-5 mx-auto w-4/5">
+    <div className="mx-auto w-4/5 ">
+      <div className="card card-side bg-base-100 shadow-lg rounded-lg my-5 mx-auto w-4/5">
         <figure>
-          <img
-            src={doc.img}
-            alt="doc"
-          />
+          <img src={doc.img} alt="doc" />
         </figure>
         <div className="card-body">
           <h2 className="card-title">{doc.name}</h2>
@@ -19,6 +16,14 @@ const DocDetailsShow = ({ doc }) => {
             <button className="btn btn-outline">{doc.specialist2}</button>
           </div>
         </div>
+      </div>
+      <div className="my-5">
+        <p className="text-2xl font-bold">About Me</p>
+        <p> {doc?.about}</p>
+      </div>
+      <div className="my-5">
+        <p className="text-2xl font-bold">Education</p>
+        <p> {doc?.education}</p>
       </div>
     </div>
   );
