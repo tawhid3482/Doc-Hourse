e4c6ed4d95937570cd545ddfdae418a04215dba7
saikat <tawhidulislam3482@gmail.com>
2024-02-27 18:26:44 +0600

    Doc detaisl show complete

--- src/Home/Doctor/DocDetailsShow.jsx
+++ src/Home/Doctor/DocDetailsShow.jsx
@@ -1,20 +1,27 @@
-
-const DocDetailsShow = ({doc}) => {
-    // console.log(doc)
-    return (
-        <div>
-            <div className="card card-side bg-base-100 shadow-xl">
-  <figure><img src="https://daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg" alt="Movie"/></figure>
-  <div className="card-body">
-    <h2 className="card-title">New movie is released!</h2>
-    <p>Click the button to watch on Jetflix app.</p>
-    <div className="card-actions justify-end">
-      <button className="btn btn-primary">Watch</button>
-    </div>
-  </div>
-</div>
+const DocDetailsShow = ({ doc }) => {
+  // console.log(doc)
+  return (
+    <div>
+      <div className="card card-side bg-base-100 shadow-xl my-5 mx-auto w-4/5">
+        <figure>
+          <img
+            src={doc.img}
+            alt="doc"
+          />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">{doc.name}</h2>
+          <p>{doc.title}</p>
+          <p>{doc.rating}</p>
+          <p>{doc.location}</p>
+          <div className="card-actions ">
+            <button className="btn btn-outline">{doc.specialist}</button>
+            <button className="btn btn-outline">{doc.specialist2}</button>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default DocDetailsShow;
