c996420200dd20ab003940ddc71b5fdb88820754
saikat <tawhidulislam3482@gmail.com>
2024-02-27 18:49:37 +0600

    private provider complete

--- src/Routes/PrivateProvider.jsx
+++ src/Routes/PrivateProvider.jsx
@@ -1,10 +1,16 @@
+import { Navigate, useLocation } from "react-router-dom";
+import UseAuth from "../Hooks/UseAuth";
 
-const PrivateProvider = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+const PrivateProvider = ({ children }) => {
+  const { user, loading } = UseAuth();
+  const location = useLocation();
+  if (loading) {
+    return <progress className="progress w-56"></progress>;
+  }
+  if (user) {
+    return children;
+  }
+  return <Navigate to="/login" state={{ from: location }} replace></Navigate>;
 };
 
 export default PrivateProvider;
