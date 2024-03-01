703b8b0a9860c13375eec23350bef1a9e0225fc9
saikat <tawhidulislam3482@gmail.com>
2024-03-01 18:54:42 +0600

    calender is working

---
+++ src/Appointment/Calender.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+import Calendar from 'react-calendar';
+
+type ValuePiece = Date | null;
+
+type Value = ValuePiece | [ValuePiece, ValuePiece];
+const Calender = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Calender;
