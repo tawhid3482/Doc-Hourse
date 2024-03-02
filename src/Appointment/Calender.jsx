3ac3812db70c1a5b846f79276223dcb324bfeb0c
saikat <tawhidulislam3482@gmail.com>
2024-03-02 18:36:57 +0600

    calender is complete

--- src/Appointment/Calender.jsx
+++ src/Appointment/Calender.jsx
@@ -1,17 +1,19 @@
 import { useState } from "react";
 import Calendar from "react-calendar";
 import 'react-calendar/dist/Calendar.css';
-
+import chair from '../assets/images/chair 1.png'
 const Calender = () => {
   const [value, onChange] = useState(new Date()) ;
 
   return (
-    <div>
+    <div className="p-10 my-10">
       <div className="flex justify-between items-center">
         <div className="">
           <Calendar onChange={onChange} value={value} />
         </div>
-        <div className=""></div>
+        <div className="">
+          <img src={chair} className="" alt="" />
+        </div>
       </div>
     </div>
   );
