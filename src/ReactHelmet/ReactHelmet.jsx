d8e27b51ddd4b9cf179ab9b670dadd274d037728
saikat <tawhidulislam3482@gmail.com>
2024-03-02 18:55:23 +0600

    react helmet async

---
+++ src/ReactHelmet/ReactHelmet.jsx
@@ -0,0 +1,14 @@
+import { Helmet } from 'react-helmet-async';
+
+const ReactHelmet = ({title}) => {
+    return (
+        <div>
+            <Helmet> 
+                
+            </Helmet>
+            <title>{title}</title>
+        </div>
+    );
+};
+
+export default ReactHelmet;
--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,15 @@ import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import Routes from "./Routes/Routes.jsx";
 import AuthProvider from "./Provider/AuthProvider.jsx";
+import {  HelmetProvider } from "react-helmet-async";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <div className="max-w-screen-xl mx-auto">
       <AuthProvider>
-        <RouterProvider router={Routes} />
+        <HelmetProvider>
+          <RouterProvider router={Routes} />
+        </HelmetProvider>
       </AuthProvider>
     </div>
   </React.StrictMode>
