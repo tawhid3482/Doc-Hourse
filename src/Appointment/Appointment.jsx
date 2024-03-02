de644a17fc3ec66c7ee42d5d2fffcba55086aabc
saikat <tawhidulislam3482@gmail.com>
2024-03-02 18:47:56 +0600

    service working

--- src/Appointment/Appointment.jsx
+++ src/Appointment/Appointment.jsx
@@ -1,11 +1,13 @@
 import Banner from "./Banner";
 import Calender from "./Calender";
+import Services from "./Services";
 
 const Appointment = () => {
     return (
         <div>
             <Banner></Banner>
             <Calender></Calender>
+            <Services></Services>
         </div>
     );
 };
---
+++ src/Appointment/Services.jsx
@@ -0,0 +1,13 @@
+import SectionTitle from "../Shared/SectionTitle/SectionTitle";
+
+const Services = () => {
+    return (
+        <div>
+         <div className="my-8">
+         <SectionTitle title={"Please select a service."}></SectionTitle>
+         </div>
+        </div>
+    );
+};
+
+export default Services;
