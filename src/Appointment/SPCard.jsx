61b5973456394e3de01963c32a4b08cc7ce3e609
saikat <tawhidulislam3482@gmail.com>
2024-03-05 16:18:02 +0600

    doctor service card working

--- src/Appointment/SPCard.jsx
+++ src/Appointment/SPCard.jsx
@@ -1,21 +1,22 @@
 const SPCard = ({ service }) => {
   console.log(service);
 
+
   return (
     <div>
       <div className="card w-96 bg-base-100 shadow-xl">
         <figure className="px-10 pt-10">
           <img
-            src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
-            alt="Shoes"
+            src={service.img}
+            alt={service.name}
             className="rounded-xl"
           />
         </figure>
         <div className="card-body items-center text-center">
-          <h2 className="card-title">Shoes!</h2>
-          <p>If a dog chews shoes whose shoes does he choose?</p>
+          <h2 className="card-title">{service.name}</h2>
+          <p>{service.time}</p>
           <div className="card-actions">
-            <button className="btn btn-primary">Buy Now</button>
+            <button className="btn btn-error btn-outline">Book Appointment</button>
           </div>
         </div>
       </div>
