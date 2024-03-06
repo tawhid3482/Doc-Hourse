49ea8bf78bb701d5805b9f78a1196c4b4b23e14c
saikat <tawhidulislam3482@gmail.com>
2024-03-06 12:58:46 +0600

    error fix

--- src/Appointment/Services.jsx
+++ src/Appointment/Services.jsx
@@ -13,7 +13,7 @@ const Services = () => {
       <div className="my-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 ">
         {service?.map((services) => (
           <div key={services.id}>
-            <Link to={`services/${services.name}`}>
+            <Link to={`/services/${services.name}`}>
             <div className="bg-gray-100 rounded-xl flex justify-center items-center gap-4 p-5">
                 <div className="">
                     <img src={services.img} className="w-32 h-32" alt="" />
