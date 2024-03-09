9212ed39c2e1d615264d3462ac001f74d2f48525
saikat <tawhidulislam3482@gmail.com>
2024-03-09 18:29:33 +0600

    useAxiosSecure  hook

--- src/Axios/UseAxiosSecure.jsx
+++ src/Axios/UseAxiosSecure.jsx
@@ -4,11 +4,36 @@ const axiosSecure = axios.create({
     baseURL: "http://localhost:5000",
   });
 const UseAxiosSecure = () => {
-    return (
-        <div>
-            <p>axios secure</p>
-        </div>
-    );
+    const navigate = useNavigate()
+   const {logoutUser}=AuthHook()
+  axiosSecure.interceptors.request.use(
+    function (config) {
+      const token = localStorage.getItem('access-token')
+      // console.log('saikat',token)
+      config.headers.authorization = `Bearer ${token}`
+      // Do something before request is sent
+      return config;
+    },
+    function (error) {
+      // Do something with request error
+      return Promise.reject(error);
+    }
+  );
+
+  axiosSecure.interceptors.response.use(function (response) {
+   
+   return response;
+ }, async (error)=> {
+   const status = error.response.status
+   console.log('error status',status)
+   if( status === 401 || status === 403){
+     await logoutUser()
+      navigate('/login')
+   }
+   return Promise.reject(error);
+ });
+
+  return axiosSecure;
 };
 
 export default UseAxiosSecure;
