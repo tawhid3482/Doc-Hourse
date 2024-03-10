f98ce6d261842ec92013157372bf0574d63114fb
saikat <tawhidulislam3482@gmail.com>
2024-03-10 11:53:03 +0600

    user admin complete

--- src/Hooks/UseAdmin.jsx
+++ src/Hooks/UseAdmin.jsx
@@ -1,10 +1,19 @@
+import { useQuery } from "@tanstack/react-query";
+import UseAxiosSecure from "../Axios/UseAxiosSecure";
+import UseAuth from "./UseAuth";
 
 const UseAdmin = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+    const {user,loading} = UseAuth()
+    const axiosSecure = UseAxiosSecure()
+    const {data: isAdmin, isPending: isAdminLoading}=useQuery({
+        queryKey:[user?.email, 'isAdmin'],
+        enabled: !loading,
+        queryFn: async()=>{
+            const res = await axiosSecure.get(`/users/admin/${user.email}`)
+            return res.data?.admin
+        }
+    })
+    return [isAdmin,isAdminLoading]
 };
 
 export default UseAdmin;
