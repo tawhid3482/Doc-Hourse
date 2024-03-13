ddb248f0e8fc5d6092fd4e668d42e17877348b99
saikat <tawhidulislam3482@gmail.com>
2024-03-13 12:57:13 +0600

    all user   working

--- src/Dashboard/AllUsers/AllUser.jsx
+++ src/Dashboard/AllUsers/AllUser.jsx
@@ -1,13 +1,63 @@
 import SectionTitle from "../../Shared/SectionTitle/SectionTitle";
 
 const AllUser = () => {
-    return (
-        <div>
-             <div className="text-center my-4">
+  const axiosSecure = UseAxiosSecure();
+  const { data, refetch } = useQuery({
+    queryKey: ["users"],
+    queryFn: async () => {
+      const res = await axiosSecure.get("/users");
+      return res.data;
+    },
+  });
+
+  const handleAdmin = (user) => {
+    axiosSecure.patch(`/users/admin/${user._id}`).then((res) => {
+      if (res.data.modifiedCount > 0) {
+        refetch();
+        Swal.fire({
+          position: "top-end",
+          icon: "success",
+          title: "Your work has been saved",
+          showConfirmButton: false,
+          timer: 1500,
+        });
+      }
+    });
+  };
+
+  const handleDelete = (id) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        axiosSecure.delete(`/users/${id}`).then((res) => {
+          if (res.data.deletedCount > 0) {
+            refetch();
+            Swal.fire({
+              title: "Deleted!",
+              text: "Your file has been deleted.",
+              icon: "success",
+            });
+          }
+        });
+      }
+    });
+  };
+
+  return (
+    <div>
+      <div className="text-center my-4">
         <SectionTitle title={"All Users"}></SectionTitle>
       </div>
-        </div>
-    );
+
+    </div>
+  );
 };
 
 export default AllUser;
