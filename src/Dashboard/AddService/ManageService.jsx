dc071c9bfba128d87fd39ee5ba4799bea86dc7c5
saikat <tawhidulislam3482@gmail.com>
2024-03-13 13:07:26 +0600

    mananage  complete

--- src/Dashboard/AddService/ManageService.jsx
+++ src/Dashboard/AddService/ManageService.jsx
@@ -1,13 +1,39 @@
 import SectionTitle from "../../Shared/SectionTitle/SectionTitle";
 
 const ManageService = () => {
-    return (
-        <div>
-            <div className="text-center my-4">
+    const [data,refetch] = UseCourse();
+  const axiosSecure = UseAxiosSecure()
+  const handleDelete = (id) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        axiosSecure.delete(`/department/${id}`).then((res) => {
+          if (res.data.deletedCount > 0) {
+            refetch()
+            Swal.fire({
+              title: "Deleted!",
+              text: "Your file has been deleted.",
+              icon: "success",
+            });
+          }
+        });
+      }
+    });
+  };
+  return (
+    <div>
+      <div className="text-center my-4">
         <SectionTitle title={"Manage Service"}></SectionTitle>
       </div>
-        </div>
-    );
+    </div>
+  );
 };
 
 export default ManageService;
