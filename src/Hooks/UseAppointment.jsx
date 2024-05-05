0c86925d739c64a5845800eb96526c114eeabf45
saikat <tawhidulislam3482@gmail.com>
2024-05-05 16:40:08 +0600

    your appointment ready

--- src/Hooks/UseAppointment.jsx
+++ src/Hooks/UseAppointment.jsx
@@ -1,16 +1,18 @@
 import { useQuery } from "@tanstack/react-query";
 import UseAxiosSecure from "../Axios/UseAxiosSecure";
+import UseAuth from "./UseAuth";
 
 const UseAppointment = () => {
     const axiosSecure = UseAxiosSecure()
-    const {data,refetch} = useQuery({
-        queryKey:['appointment'],
-        queryFn: async()=>{
-            const res = await axiosSecure.get('/appointment')
-            return res.data
-        }
-    })
-    return [data,refetch]
+    const { user } = UseAuth();
+    const { refetch, data } = useQuery({
+      queryKey: ["appointment", user?.email],
+      queryFn: async () => {
+        const res = await axiosSecure.get(`/appointment?email=${user?.email}`);
+        return res.data;
+      },
+    });
+    return [data, refetch];
 };
 
 export default UseAppointment;
