5b413fa57cfb92347ed52ef10d05f6a91eb4bc99
saikat <tawhidulislam3482@gmail.com>
2024-05-07 21:33:29 +0600

    helmet ready

--- src/Dashboard/Users/MyHistory.jsx
+++ src/Dashboard/Users/MyHistory.jsx
@@ -1,10 +1,13 @@
+import { Helmet } from "react-helmet-async";
 
 const MyHistory = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <div>
+      <Helmet>
+        <title>Doc-House | Patient History</title>
+      </Helmet>
+    </div>
+  );
 };
 
 export default MyHistory;
